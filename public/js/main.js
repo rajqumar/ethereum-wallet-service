9e80335e9ce240823387e2e85ef71f5830f246cd
rajqumar <rajteewari57@gmail.com>
2018-04-25 15:04:42 +0530

    updated lw

--- public/js/main.js
+++ public/js/main.js
@@ -1,107 +1,71 @@
+$( document ).ready(function() {
+	$('#seedDiv').hide();
+	$('#addressDiv').hide();
+});
 
+function seed_phrase(){
 
-function generate_seed(){
-	var new_seed = lightwallet.keystore.generateRandomSeed();
-	document.getElementById('seed').value = new_seed;
-	generate_addresses(new_seed);
+	var password = $('#password').val();
+	
+	var seedphrase = lightwallet.keystore.generateRandomSeed();
+
+	$('#passwordDiv').hide();
+	$('#seedDiv').show();
+	$('#seedPhrase').html(seedphrase);
 }
 
-var totalAddresses = 0;
-
-function generate_addresses(seed){
-	if(seed == undefined){
-		seed = document.getElementById('seed').value;
-	}
+function generate_address(password, seed){
+	var password = $('#password').val();
+	var seed = $('#seedPhrase').text();
 	
 	if(!lightwallet.keystore.isSeedValid(seed)){
-		document.getElementById("info").innerHTML = "Please enter a valid seed";
-		return;
-	}
-	
-	totalAddresses = prompt('How many addresses do you want to generate ?');
-
-	if(!Number.isInteger(parseInt(totalAddresses))){
-		document.getElementById("info").innerHTML = "Please enter valid number of addresses";
-		return;
+		alert('Please enter a valid seed');
 	}
 
-	var password = Math.random().toString();
+	console.log("password:" + password + " Seed: " + seed);
 
-	lightwallet.keystore.createVault({password:password, seedPhrase: seed}, function(err, ks){
+	lightwallet.keystore.createVault({
+		password: password,
+		seedPhrase: seed
+	}, function(err, ks){
+		if(err) alert(err);
 		ks.keyFromPassword(password, function(err, pwDerivedKey){
-			if(err) {
-				document.getElementById('info').innerHTML = err;
-			}else{
-				ks.generateNewAddress(pwDerivedKey, totalAddresses);
-				var addresses = ks.getAddresses();
-				
-				var web3 = new Web3(new Web3.providers.HttpProvider("https://kovan.infura.io/GOLwPdGco4MXYgisAa50"));
+			if(err) alert(err);
+			ks.generateNewAddress(pwDerivedKey, 1);
+			var addr = ks.getAddresses();
+			// var exp = lightwallet.keystore.exportKey(addr, pwDerivedKey);
+			// console.log(exp);
+			console.log(addr);
+			$('#passwordDiv').hide();
+			$('#seedDiv').show();
 
-				var html = '';
+			var web3 = new Web3(new Web3.providers.HttpProvider("https://kovan.infura.io/GOLwPdGco4MXYgisAa50"));
+			console.log(web3);
 
-				for(var count = 0; count < addresses.length; count++){
-					var address = addresses[count];
-					var private_key = ks.exportPrivateKey(address, pwDerivedKey);
-					var balance = web3.eth.getBalance("0x"+address);
+			var private_key = ks.exportPrivateKey(addr, pwDerivedKey);
+			var balance = web3.eth.getBalance("0x"+addr);
+			var address = "0x" + addr;
+			console.log("PK: " + private_key + "Balance: " + balance);	
+		
+			$('#addressDiv').show();
+			$('#seedDiv').hide();
+			$('#info').html('Your wallet information');	
+			$('#wallet_addr').text(address);
+			$('#balance').text(balance);
+			$('#private_key').html("<strong>Private Key: </strong>" + private_key);
+			$('#private_key').hide();
+			// html = html + "<li>";
+			// html = html + "<p><b>Address: </b>0x" + addr + "</p>";
+			// html = html + "<p><b>Private Key: </b>0x" + private_key + "</p>";
+			// html = html + "<p><b>Balance: </b>" + web3.fromWei(balance, "ether") + " ether</p>";
+			// html = html + "</li>";
 
-					html = html + "<li>";
-					html = html + "<p><b>Address: </b>0x" + address + "</p>";
-					html = html + "<p><b>Private Key: </b>0x" + private_key + "</p>";
-					html = html + "<p><b>Balance: </b>" + web3.fromWei(balance, "ether") + " ether</p>";
-					html = html + "</li>";
-				}
-
-				document.getElementById("list").innerHTML = html;
-			}
-		});	
-	});
-}
-
-function send_ether(){
-	var seed = document.getElementById("seed").value;
-
-	if(!lightwallet.keystore.isSeedValid(seed)){
-		document.getElementById("info").innerHTML = "Please enter a valid seed";
-		return;
-	}
-
-	var password = Math.random().toString();
-
-	lightwallet.keystore.createVault({password:password, seedPhrase:seed}, function(err, ks){
-		ks.keyFromPassword(password, function(err, pwDerivedKey){
-			if(err){
-				document.getElementById("info").innerHTML = err;
-			}else{
-				ks.generateNewAddress(pwDerivedKey, totalAddresses);
-					ks.passwordProvider = function (callback){
-						callback(null, password);
-					};
-
-					var provider = new HookedWeb3Provider({
-						host: "https://kovan.infura.io/GOLwPdGco4MXYgisAa50",
-						transaction_signer: ks
-					});
-
-					var web3 = new Web3(provider);
-
-					var from = document.getElementById("address1").value;
-					var to   = document.getElementById("address2").value;
-					var value= web3.fromWei(document.getElementById("ether").value, "ether");
-
-					web3.eth.sendTransaction({
-						from: from,
-						to: to,
-						value: value,
-						gas: 21000
-						},function(error, result){
-							if(error){
-								document.getElementById("info").innerHTML = error;
-							}else{
-								document.getElementById("info").innerHTML = "Txn hash: " + result;
-							}
-						});
-				
-			}
+			// $('#addressText').html(html);
 		});
 	});
+}
+
+function private_key_gen(){
+	$('#private_key').show();
+	$('#pkbutton').hide();
 }
